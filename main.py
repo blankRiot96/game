ff34370dc20f0fb3467e3e90af6e7942cba56220
blankRiot96 <blankRiot96@gmail.com>
2024-12-14 11:01:07 +0530

    added fps tracker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from loguru import logger
 
 from scripts.event_dispatchers import get_clock, get_dt, get_events
+from scripts.fps_tracker import display_mean_fps
 from scripts.state_dispatcher import dispatch_state, process_state
 from src.state_enums import GameState
 
@@ -9,8 +10,6 @@ from src.state_enums import GameState
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 800))
-    clock = get_clock()
-    fps_font = pygame.Font(size=32)
 
     new_state = GameState.GAME
     state_processor = None
@@ -25,7 +24,7 @@ def main():
         screen.fill("black")
 
         new_state, state_processor = process_state(new_state, screen, state_processor)
-        screen.blit(fps_font.render(f"{clock.get_fps():.0f}", True, "green"), (0, 0))
+        display_mean_fps(screen, seconds=1)
         pygame.display.flip()
 
 
---
+++ scripts/fps_tracker.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.typing import Point
+
+from scripts.event_dispatchers import get_clock
+from scripts.timers import Time
+
+pygame.font.init()
+fps_font = pygame.Font(size=32)
+clock = get_clock()
+
+
+def get_fps_surf(fps: float | None = None):
+    if fps is None:
+        fps = clock.get_fps()
+    return fps_font.render(f"{fps:.0f}", True, "green")
+
+
+class _FrameInfo:
+    fps_in_prev_frames: list[int] = []
+    fps_surf: pygame.Surface = get_fps_surf()
+    fps_refresh_timer = Time(1.0)
+
+
+def display_current_fps(screen: pygame.Surface, dest: Point = (0, 0)):
+    screen.blit(get_fps_surf(), dest)
+
+
+def display_mean_fps(
+    screen: pygame.Surface, seconds: float, dest: Point = (0, 0)
+) -> None:
+    _FrameInfo.fps_refresh_timer.time_to_pass = seconds
+    _FrameInfo.fps_in_prev_frames.append(clock.get_fps())
+    if _FrameInfo.fps_refresh_timer.tick():
+        mean_fps = sum(_FrameInfo.fps_in_prev_frames) / len(
+            _FrameInfo.fps_in_prev_frames
+        )
+        _FrameInfo.fps_surf = get_fps_surf(mean_fps)
+        _FrameInfo.fps_in_prev_frames.clear()
+
+    screen.blit(_FrameInfo.fps_surf, dest)
